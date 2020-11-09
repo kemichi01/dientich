75f4e0d81ed7bb959c5f012c172558b1cb883e61
unknown <kemichi01@gmail.com>
2020-11-09 17:18:06 +0700

    dientichla

---
+++ taontu.js
@@ -0,0 +1,8 @@
+var width = "chiều rộng"
+var height = "chiều dài"
+var width = prompt('nhap chieu dai')
+var height = prompt('nhap chieu rộng')
+var width = parseInt(width)
+var height = parseInt(height)
+var s = width*height
+document.write("s =" + s)
